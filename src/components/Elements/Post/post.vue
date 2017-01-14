bb7a4438f61d7cc0e4256ecf8d11836bf78e662f
Ricki Moore <rickimoore@Rickis-MacBook-Air.local>
2017-01-15 00:54:53 +0100

    added the blog post layout

---
+++ src/components/Elements/Post/post.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="post">
+        <h1>{{title}}</h1>
+        <p>{{description}}</p>
+    </div>
+</template>
+<script>
+    export default{
+        props: ['title', 'description', 'image'],
+        mounted(){
+            console.log('ready post');
+        }
+    }
+</script>
--- src/components/Views/Landing.vue
+++ src/components/Views/Landing.vue
@@ -1,12 +1,17 @@
 <template>
     <div class="#landing">
         <h1>This is the Landing Page</h1>
+        <posts></posts>
     </div>
 </template>
 <script>
+    import posts from '../Layouts/Posts.vue';
     export default{
         mounted(){
             console.log('working correctly');
+        },
+        components: {
+            posts
         }
     }
 </script>

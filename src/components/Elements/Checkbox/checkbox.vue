704363ba12feba70326d794efdf83efbf69164b0
Kevin Ivan <kevin.ivan@oakwood.se>
2017-01-15 00:53:27 +0100

    add stuff

---
+++ src/components/Elements/Checkbox/checkbox.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="checkbox" @click="toggle">
+        <span :class="{'is-checked': isChecked}">{{label}}</span>
+    </div>
+</template>
+<script>
+    export default{
+        props: ['label'],
+        mounted(){
+            console.log('now we cooking');
+        },
+        data: function() {
+            return {
+                isChecked: false
+            }
+        },
+        methods:{
+            toggle: function(){
+                this.isChecked = !this.isChecked;
+            }
+        }
+    }
+</script>
--- src/components/Views/Register.vue
+++ src/components/Views/Register.vue
@@ -1,12 +1,25 @@
 <template>
-    <div class="register">
+    <main class="register">
         <h1>This is the registration View.</h1>
-    </div>
+        <h2>title</h2>
+        <section>
+            <checkbox v-for="(check, index) in checkboxesInterests" :label="check"></checkbox>
+        </section>
+    </main>
 </template>
 <script>
+    import checkbox from '../Elements/Checkbox/checkbox.vue';
     export default{
         mounted(){
             console.log('this is the register page');
+        },
+        data: function(){
+            return {
+                checkboxesInterests: ['Engineer', 'Design', 'Data Science', 'Product Management', 'Event Planning', 'Fasion', 'Marketing', 'Finance', 'Other']
+            }
+        },
+        components: {
+            checkbox
         }
     }
 </script>
